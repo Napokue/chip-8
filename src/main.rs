90d8d6b3f10ee4fce5573bc5e8194743ea24e66e
Timo de Kort <dekort.timo@gmail.com>
2019-10-30 22:26:53 +0100

    start project

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let opcode: u16;
+    let memory: [char; 4096];
+    let v: [char; 16];
+    let i: u16;
+    let pc: u16;
+    let gfx: [char; 64 * 32];
+    let delay_timer: char;
+    let sound_timer: char;
+    let stack: u16;
+    let sp: u16;
+    let key: [char; 16];
+}
