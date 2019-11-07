44f9e01c30773d87677952128e3c4be52a765f38
Timo de Kort <dekort.timo@gmail.com>
2019-11-07 20:25:11 +0100

    implement nible match

--- src/cpu.rs
+++ src/cpu.rs
@@ -45,181 +45,258 @@ impl Cpu {
     pub fn emulate_cycle(&mut self) {
         let opcode = (self.memory[self.pc] as u16) << 8 | (self.memory[self.pc + 1] as u16);
 
-        let nnn = (opcode & 0x0FFF);
-        let kk = (opcode & 0x00FF) as u8;
-        let x = (opcode & 0x0F00) >> 8 as u8;
-        let y = (opcode & 0x00F0) >> 4 as u8;
-        let n = (opcode & 0x00F) as u8;
-
         println!("opcode: {}", opcode);
 
-        match opcode & 0x0F000 {
-            0x0000 => {
-                println!("0x0000");
-                match opcode & 0x000F {
-                    0x0000 => { // 0x00E0
-                        println!("0x0000");
-                        for y in 0..self.gfx.len() {
-                            for x in 0..self.gfx[y].len() {
-                                self.gfx[y][x] = 0;
-                            }
-                        }
+        // Credits to https://github.com/starrhorne/chip8-rust for this nibble match implementation
+        let nibbles = (
+            (opcode & 0xF000) >> 12 as u8,
+            (opcode & 0x0F00) >> 8 as u8,
+            (opcode & 0x00F0) >> 4 as u8,
+            (opcode & 0x000F) as u8,
+        );
 
-                        self.next_instruction();
-                    },
-                    0x000E => { // 0x00EE
-                        println!("0x000E");
-                        self.jump_to_instruction(self.stack[self.sp] as usize);
-                        self.sp -= 1;
-                    },
-                    _ => println!("Unknown opcode: {}", opcode)
+        let nnn = (opcode & 0x0FFF);
+        let kk = (opcode & 0x00FF) as u8;
+        let x = nibbles.1 as u8;
+        let y = nibbles.2 as u8;
+        let n = nibbles.3 as u8;
+
+        match nibbles {
+            // 00E0 - CLS
+            (0x00, 0x00, 0x0E, 0x00) => {
+                println!("00E0");
+                for y in 0..self.gfx.len() {
+                    for x in 0..self.gfx[y].len() {
+                        self.gfx[y][x] = 0;
+                    }
                 }
+
+                self.next_instruction();
             },
-            0x1000 => { // 0x1NNN
-                println!("0x0000");
+            // 00EE - RET
+            (0x00, 0x00, 0x0E, 0x0E) => {
+                println!("00EE");
+                self.jump_to_instruction(self.stack[self.sp] as usize);
+                self.sp -= 1;
+            },
+            // 1nnn - JP addr
+            (0x01, _, _, _) => {
+                println!("1nnn");
                 self.jump_to_instruction(nnn as usize);
             },
-            0x2000 => { // 0x2NNN
-                println!("0x2000");
+            // 2nnn - CALL addr
+            (0x02, _, _, _) => {
+                println!("2nnn");
                 self.sp += 1;
                 self.stack[self.sp] = self.pc as u16;
                 self.jump_to_instruction(nnn as usize);
             },
-            0x3000 => { // 0x3NNN
-                println!("0x3000");
+            // 3xkk - SE Vx, byte
+            (0x03, _, _, _) => {
+                println!("3xkk");
                 if self.v[x as usize] == kk {
                     self.skip_instruction();
                 } else {
                     self.next_instruction();
                 }
             },
-            0x4000 => { // 0x4NNN
-                println!("0x4000");
+            // 4xkk - SNE Vx, byte
+            (0x04, _, _, _) => {
+                println!("4xkk");
                 if self.v[x as usize] != kk {
                     self.skip_instruction();
                 } else {
                     self.next_instruction();
                 }
             },
-            0x5000 => { // 0x5XY0
-                println!("0x5000");
+            // 5xy0 - SE Vx, Vy
+            (0x05, _, _, 0x00) => {
+                println!("5xy0");
                 if self.v[x as usize] == self.v[y as usize] {
                     self.skip_instruction();
                 } else {
                     self.next_instruction();
                 }
             },
-            0x6000 => { // 0x6XNN
-                println!("0x6000");
+            // 6xkk - LD Vx, byte
+            (0x06, _, _, _) => {
+                println!("6xkk");
                 self.v[x as usize] = kk;
                 self.next_instruction();
             },
-            0x7000 => { // 0x7XNN
-                println!("0x7000");
+            // 7xkk - ADD Vx, byte
+            (0x07, _, _, _) => {
+                println!("7xkk");
                 self.v[x as usize] = self.v[x as usize] + kk;
                 self.next_instruction();
             },
-            0x8000 => {
-                println!("0x8000");
-                match opcode & 0x000F {
-                    0x8001 => {
-                        println!("0x8001");
-                        self.v[x as usize] = self.v[x as usize] | self.v[y as usize];
-                        self.next_instruction();
-                    },
-                    0x8002 => {
-                        println!("0x8002");
-                        self.v[x as usize] = self.v[x as usize] & self.v[y as usize];
-                        self.next_instruction();
-                    },
-                    0x8003 => {
-                        println!("0x8003");
-                        self.v[x as usize] = self.v[x as usize] ^ self.v[y as usize];
-                        self.next_instruction();
-                    },
-                    0x8004 => {
-                        // TODO
-                        println!("0x8004");
-                        self.next_instruction();
-                    },
-                    0x8005 => {
-                        println!("0x8005");
-                        if self.v[x as usize] > self.v[y as usize] {
-                            self.v[0xF] = 1;
-                        } else {
-                            self.v[0xF] = 0;
-                        }
-
-                        self.v[x as usize] = self.v[x as usize] - self.v[y as usize];
-                        self.next_instruction();
-                    },
-                    0x8006 => {
-                        // TODO
-                        println!("0x8006");
-                        self.next_instruction();
-                    },
-                    0x8007 => {
-                        println!("0x8007");
-                        if self.v[y as usize] > self.v[x as usize] {
-                            self.v[0xF] = 1;
-                        } else {
-                            self.v[0xF] = 0;
-                        }
-
-                        self.v[x as usize] = self.v[y as usize] - self.v[x as usize];
-                        self.next_instruction();
-                    },
-                    0x800E => {
-                        // TODO
-                        println!("0x800E");
-                        self.next_instruction();
-                    },
-                    _ => println!("0x8000 opcode: {}", opcode)
-                }                
+            // 8xy0 - LD Vx, Vy
+            (0x08, _, _, 0x00) => {
+                println!("8xy0");
+                self.v[x as usize] = self.v[y as usize];
+                self.next_instruction();
             },
-            0x9000 => { // 0x9XY0
-                println!("0x9000");
+            // 8xy1 - OR Vx, Vy
+            (0x08, _, _, 0x01) => {
+                println!("8xy1");
+                self.v[x as usize] = self.v[x as usize] | self.v[y as usize];
+                self.next_instruction();
+            },
+            // 8xy2 - AND Vx, Vy
+            (0x08, _, _, 0x02) => {
+                println!("8xy2");
+                self.v[x as usize] = self.v[x as usize] & self.v[y as usize];
+                self.next_instruction();
+            },
+            // 8xy3 - XOR Vx, Vy
+            (0x08, _, _, 0x03) => {
+                println!("8xy3");
+                self.v[x as usize] = self.v[x as usize] ^ self.v[y as usize];
+                self.next_instruction();
+            },
+            // 8xy4 - ADD Vx, Vy
+            (0x08, _, _, 0x04) => {
+                // TODO
+                println!("8xy4");
+                self.next_instruction();
+            },
+            // 8xy5 - SUB Vx, Vy
+            (0x08, _, _, 0x05) => {
+                println!("8xy5");
+                if self.v[x as usize] > self.v[y as usize] {
+                    self.v[0xF] = 1;
+                } else {
+                    self.v[0xF] = 0;
+                }
+
+                self.v[x as usize] = self.v[x as usize] - self.v[y as usize];
+                self.next_instruction();
+            },
+            // 8xy6 - SHR VX, Vy
+            (0x08, _, _, 0x06) => {
+                // TODO
+                println!("8xy6");
+                self.next_instruction();
+            },
+            // 8xy7 - SUBN Vx, Vy
+            (0x08, _, _, 0x07) => {
+                println!("8xy7");
+                if self.v[y as usize] > self.v[x as usize] {
+                    self.v[0xF] = 1;
+                } else {
+                    self.v[0xF] = 0;
+                }
+
+                self.v[x as usize] = self.v[y as usize] - self.v[x as usize];
+                self.next_instruction();
+            },
+            // 8xyE - SHL Vx, Vy
+            (0x08, _, _, 0x0E) => {
+                // TODO
+                println!("8xyE");
+                self.next_instruction();
+            },
+            // 9xy0 - SNE Vx, Vy
+            (0x09, _, _, 0x00) => {
+                println!("9xy0");
                 if self.v[x as usize] != self.v[y as usize] {
                     self.skip_instruction();
                 } else {
                     self.next_instruction();
                 }
-            }
-            0xA000 => { // 0xANNN                
-                println!("0xA000");
+            },
+            // Annn - LD I, addr
+            (0x0A, _, _, _) => {
+                println!("Annn");
                 self.i = nnn as usize;
                 self.next_instruction();
             },
-            0xB000 => { // 0xBNNN
-                println!("0xB000");
+            // Bnnn - JP V0, addr
+            (0x0B, _, _, _) => {
+                println!("Bnnn");
                 self.pc += nnn as usize + self.v[0] as usize;
             },
-            0xC000 => { // 0xCXNN
-                println!("0xC000");
+            // Cxkk - RND Vx, byte
+            (0x0C, _, _, _) => {
+                println!("Cxkk");
                 self.v[x as usize] = 
                     rand::thread_rng().gen_range(0, 255) &
                     kk;
 
                 self.next_instruction();
             },
-            0xD000 => { // 0xDXYN
+            // Dxyn - DRW Vx, Vy, nibble
+            (0x0D, _, _, _) => {
                 // TODO
-                println!("0xD000");
+                println!("Dxyn");
                 self.next_instruction();
             },
-            0xE000 => {
-                println!("0xE000");
-                match opcode & 0x000F {
-                    _ => println!("0xE000 opcode: {}", opcode)
-                }   
+            // Ex9E - SKP Vx
+            (0x0E, _, 0x09, 0x0E) => {
+                // TODO
+                println!("Ex9E");
+                self.next_instruction();
             },
-            0xF000 => {
-                println!("0xF000");
-                match opcode & 0x000F {
-                    _ => println!("0xF000 opcode: {}", opcode)
-                } 
+            // ExA1 - SKNP Vx
+            (0x0E, _, 0x0A, 0x01) => {
+                // TODO
+                println!("ExA1");
+                self.next_instruction();
             },
-            _ => println!("Unknown opcode: {}", opcode)
+            // Fx07 - LD Vx, DT
+            (0x0F, _, 0x00, 0x07) => {
+                // TODO
+                println!("Fx07");
+                self.next_instruction();
+            },
+            // Fx0A - LD Vx, K
+            (0x0F, _, 0x00, 0x0A) => {
+                // TODO
+                println!("Fx0A");
+                self.next_instruction();
+            },
+            // Fx15 - LD DT, Vx
+            (0x0F, _, 0x01, 0x05) => {
+                // TODO
+                println!("Fx15");
+                self.next_instruction();
+            },
+            // Fx18 - LD ST, Vx
+            (0x0F, _, 0x01, 0x08) => {
+                // TODO
+                println!("Fx18");
+                self.next_instruction();
+            },
+            // Fx1E - ADD I, Vx
+            (0x0F, _, 0x01, 0x0E) => {
+                // TODO
+                println!("Fx1E");
+                self.next_instruction();
+            },
+            // Fx29 - LD F, Vx
+            (0x0F, _, 0x02, 0x09) => {
+                // TODO
+                println!("Fx29");
+                self.next_instruction();
+            },
+            // Fx33 - LD B, Vx
+            (0x0F, _, 0x03, 0x03) => {
+                // TODO
+                println!("Fx33");
+                self.next_instruction();
+            },
+            // Fx55 - LD [I], Vx
+            (0x0F, _, 0x05, 0x05) => {
+                // TODO
+                println!("Fx55");
+                self.next_instruction();
+            },
+            // Fx65 - LD Vx, [I]
+            (0x0F, _, 0x06, 0x05) => {
+                // TODO
+                println!("Fx65");
+                self.next_instruction();
+            },
         }
 
         if self.delay_timer > 0 {
@@ -230,7 +307,7 @@ impl Cpu {
             self.sound_timer -= 1;
         }
 
-        thread::sleep(time::Duration::from_secs(1))
+        thread::sleep(time::Duration::from_millis(500))
     }
 
     fn next_instruction(&mut self) {
