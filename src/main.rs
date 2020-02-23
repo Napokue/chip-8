15e71b0c3c72f47dfec8dacf847125c89ce63863
Timo de Kort <dekort.timo@gmail.com>
2020-02-23 19:07:14 +0100

    remove game loop in the main function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::{
     boilerplate::Application
 };
+
 mod cpu;
 
 use cpu::Cpu;
@@ -13,197 +14,12 @@ mod boilerplate;
 const CPU_DELAY : u64 = 5000; // microseconds
 
 mod engines;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut cpu = Cpu::new(&args[1]);
-    let mut gpu = GPU::new();    
 
-    let mut events_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_inner_size(LogicalSize::new(WINDOW_SIZE.0, WINDOW_SIZE.1))
-        .build(&events_loop)
-        .unwrap();
-
-    events_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
-        cpu.emulate_cycle();
-
-        match event {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-
-                WindowEvent::KeyboardInput {
-                    input:
-                        KeyboardInput {
-                            virtual_keycode: Some(virtual_code),
-                            state,
-                            ..
-                        },
-                    ..
-                } => match (virtual_code, state) {
-                    (VirtualKeyCode::Key1, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed Key1");
-                            cpu.key[0x1] = 1;
-                        } else {
-                            println!("Released Key1");
-                            cpu.key[0x1] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::Key2, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed Key2");
-                            cpu.key[0x2] = 1;
-                        } else {
-                            println!("Released Key2");
-                            cpu.key[0x2] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::Key3, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed Key3");
-                            cpu.key[0x3] = 1;
-                        } else {
-                            println!("Released Key3");
-                            cpu.key[0x3] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::Key4, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed Key4");
-                            cpu.key[0xC] = 1;
-                        } else {
-                            println!("Released Key4");
-                            cpu.key[0xC] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::Q, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed Q");
-                            cpu.key[0x4] = 1;
-                        } else {
-                            println!("Released Q");
-                            cpu.key[0x4] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::W, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed W");
-                            cpu.key[0x5] = 1;
-                        } else {
-                            println!("Released W");
-                            cpu.key[0x5] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::E, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed E");
-                            cpu.key[0x6] = 1;
-                        } else {
-                            println!("Released E");
-                            cpu.key[0x6] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::R, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed R");
-                            cpu.key[0xD] = 1;
-                        } else {
-                            println!("Released R");
-                            cpu.key[0xD] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::A, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed A");
-                            cpu.key[0x7] = 1;
-                        } else {
-                            println!("Released A");
-                            cpu.key[0x7] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::S, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed S");
-                            cpu.key[0x8] = 1;
-                        } else {
-                            println!("Released S");
-                            cpu.key[0x8] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::D, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed D");
-                            cpu.key[0x9] = 1;
-                        } else {
-                            println!("Released D");
-                            cpu.key[0x9] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::F, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed F");
-                            cpu.key[0xE] = 1;
-                        } else {
-                            println!("Released F");
-                            cpu.key[0xE] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::Z, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed Z");
-                            cpu.key[0xA] = 1;
-                        } else {
-                            println!("Released Z");
-                            cpu.key[0xA] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::X, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed X");
-                            cpu.key[0x0] = 1;
-                        } else {
-                            println!("Released X");
-                            cpu.key[0x0] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::C, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed C");
-                            cpu.key[0xB] = 1;
-                        } else {
-                            println!("Released C");
-                            cpu.key[0xB] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::V, _) => {
-                        if state == ElementState::Pressed {
-                            println!("Pressed V");                                
-                            cpu.key[0xF] = 1;
-                        } else {
-                            println!("Released V");
-                            cpu.key[0xF] = 0;
-                        }
-                    }
-                    (VirtualKeyCode::Escape, _) => *control_flow = ControlFlow::Exit,                       
-                    _ => (),
-                },
-                _ => (),
-            },
-            Event::MainEventsCleared  => {
-                if (cpu.draw_flag) {
-                    window.request_redraw();
-                    cpu.draw_flag = false;
-                }
     let mut harness = boilerplate::Harness::new("Chip 8 Emulator");
 
-                thread::sleep(time::Duration::from_micros(CPU_DELAY));
-            },
-            Event::RedrawRequested(_) => {
-                gpu.draw_screen(cpu.vram);
-            },
-            _ => {}
-        }            
-    });
     let renderer = engines::graphics::Engine::new();
 }
