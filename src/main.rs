7135af8c96bf5314ace0361f7ecfb6120edd43e2
Timo de Kort <dekort.timo@gmail.com>
2023-10-03 22:57:05 +0200

    feat: first steps with Bevy

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,34 @@ mod cpu;
 use cpu::Cpu;
 
 use std::{env};
+use bevy::DefaultPlugins;
+use bevy::prelude::{App, Plugin, Update};
 
 #[path = "./boilerplate.rs"]
 mod boilerplate;
 
 const CPU_DELAY : u64 = 5000; // microseconds
 
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, hello_world);
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut cpu = Cpu::new(&args[1]);
+    App::new()
+        .add_plugins((DefaultPlugins, HelloPlugin))
+        .run();
+    // let mut cpu = Cpu::new(&args[1]);
+    // let renderer = engines::graphics::Engine::new();
 
-    let renderer = engines::graphics::Engine::new();
 }
+
+fn hello_world() {
+    println!("Hello, world!");
+}
+
+
